230955cae297f7f459768dcf423c6bfd14af8a9b
swapna <swapna.h.vishwanath@gmail.com>
2021-01-27 00:47:04 +0530

    Step1: Look and feel changes for step 1

--- src/components/LoadCard.vue
+++ src/components/LoadCard.vue
@@ -1,45 +1,51 @@
 <template>
-  <div class="cards" >
-      <div >
-        <div>
+<div>
+  <div class="cards" id="cards">
+      <div id="question">
+        <h3>How do you feel about these quotes?</h3>
+      </div>
+      <div>
+        <div id="cardList">
           <div>
             <div v-if="visibleSlide === 0">
-        <img src="../assets/img_statement_1.png" alt="" style="width:50%">
+        <img src="../assets/img_statement_1.png" alt="" >
       </div>
       <div v-if="visibleSlide === 1">
-        <img src="../assets/img_statement_2.png" alt="" style="width:50%">
+        <img src="../assets/img_statement_2.png" alt="" >
       </div>
       <div v-if="visibleSlide === 2">
-        <img src="../assets/img_statement_3.png" alt="" style="width:50%">
+        <img src="../assets/img_statement_3.png" alt="" >
       </div >
       <div v-if="visibleSlide === 3">
-        <img src="../assets/img_statement_4.png" alt="" style="width:50%">
+        <img src="../assets/img_statement_4.png" alt="" >
       </div>
       <div v-if="visibleSlide === 4">
-        <img src="../assets/img_statement_5.png" alt="" style="width:50%">
+        <img src="../assets/img_statement_5.png" alt="" >
       </div>
       <div v-if="visibleSlide === 5">
-        <img src="../assets/img_statement_6.png" alt="" style="width:50%">
+        <img src="../assets/img_statement_6.png" alt="" >
       </div>
           </div>
-      <div class="nextDiv">
-        <a class="next" @click="nextCard();">&#10095;</a>
-      </div>
+      
         </div>
         <div class="smilies">
-          <a >
-            <img src="../assets/img_cat_happy.png" alt="" style="width:100px;height:100px">
+          <a @click="feedback('happy');">
+            <img src="../assets/img_cat_happy.png" alt="" style="width:100px;height:100px"/>
           </a>
-          <a>
+          <a @click="feedback('neutral');">
             <img src="../assets/img_cat_neutral.png" alt="" style="width:100px;height:100px">
           </a>
-          <a>
+          <a @click="feedback('supersad');">
             <img src="../assets/img_cat_supersad.png" alt="" style="width:100px;height:100px">
           </a>
         </div>
       </div>
       
   </div>
+  <div class="nextDiv">
+        <a class="next" @click="nextCard();">&#10095;</a>
+      </div>
+      </div>
 </template>
 
 <script>
@@ -48,7 +54,8 @@ export default {
   data() {
     return {
       cards: [],
-      visibleSlide: 0
+      visibleSlide: 0,
+      feedbackValue: null
     }
   },
   props: {
@@ -59,16 +66,24 @@ export default {
       if(this.visibleSlide<5) {
         this.visibleSlide++;
       }
+    },
+    feedback(value){
+      this.feedbackValue = value;
+      this.nextCard();
     }
   }
 }
 </script>
 
 <style lang="scss" scoped>
+  #cardList img {
+    width:90%;
+    margin: 20px;
+  }
   .smilies img{
     background-color: white;
     border-radius: 4px;
-    margin-right: 10px;
+    margin: 10px;
   }
   .nextDiv{
         font-size: 20px;
@@ -77,9 +92,23 @@ export default {
     width:30px;
     height:30px;
     text-align: center;
+    position: absolute;
+    top:25%;
+    display:inline-block;
+    margin-left: 20px;
   }
   a:hover{
     cursor: pointer;
+  }
+  #cards{
+    text-align: center;
+    width:50%;
+    background-color:yellowgreen;
+    display:inline-block;
+  }
+  #question {
+    border-bottom:0.5px solid white;
+    color:white;
   }
 </style>
 
